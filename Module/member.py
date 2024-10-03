ebce662237deb0512c251cc18e1ebed885964c7d
ArisDEV <tranthanhnam.ccc@gmail.com>
2024-10-03 20:07:13 +0700

    remove logger, dispatch some event for handling, set info to debug in function sync_name

--- Module/member.py
+++ Module/member.py
@@ -1,11 +1,10 @@
+from typing import Optional
+
 from disnake import Status, Forbidden, HTTPException
 from disnake.ext.commands import Cog
 from asyncio import sleep, Lock, Task, create_task, CancelledError
 
 from utils.ClientUser import ClientUser
-from logging import getLogger
-
-logger = getLogger(__name__)
 
 GUILDID = 1208756727323955250
 ONLINE_CHANNEL = 1234910951506182164
@@ -22,7 +21,8 @@ class MemberCount(Cog):
     def __init__(self, bot: ClientUser):
         self.bot = bot
         self.lock = Lock()
-        self.sync_tasks: Task = None # type: ignore
+        self.sync_tasks: Optional[Task] = None
+
 
     async def rename_channel(self, ChannelID = None, text = None):
         guild = self.bot.get_guild(GUILDID)
@@ -63,37 +63,44 @@ class MemberCount(Cog):
             online, dnd, idle = self.count_member()
             try:
                 await self.rename_channel(ONLINE_CHANNEL, ONLINE_TEXT.format(online=online, all=len(self.all())))
-                logger.info("START SYNC CHANNEL %s", ONLINE_CHANNEL)
+                self.bot.logger.debug("START SYNC CHANNEL %s", ONLINE_CHANNEL)
                 await sleep(3) # Sleep để tránh ratelimit
                 await self.rename_channel(IDLE_CHANNEL, IDLE_TEXT.format(idle=idle))
-                logger.info("START SYNC CHANNEL %s", IDLE_CHANNEL)
+                self.bot.logger.debug("START SYNC CHANNEL %s", IDLE_CHANNEL)
                 await sleep(3) # Sleep để tránh ratelimit
                 await self.rename_channel(DND_CHANNEL, DND_TEXT.format(dnd=dnd))
-                logger.info("START SYNC CHANNEL %s", DND_CHANNEL)
+                self.bot.logger.debug("START SYNC CHANNEL %s", DND_CHANNEL)
             except Forbidden or HTTPException as e:
-                logger.error(e)
-                self.sync_tasks.cancel("Hủy task đếm member vì đã xảy ra sự cố")
+                self.bot.logger.error(e)
+                self.bot.dispatch("sync_failed")
             except Exception as e:
-                logger.error(e)
+                self.bot.logger.error(e)
+                self.bot.dispatch("sync_failed")
 
     async def run_loop(self):
+        self.bot.logger.info("Đã nhận tín hiệu khởi động vòng lặp, Thời gian chờ mỗi lần sync: %s giây", SLEEP_TIME)
         while True:
-            logger.info("Đã nhận tín hiệu khởi động vòng lặp, Thời gian chờ mỗi lần sync: %s giây", SLEEP_TIME)
             await sleep(SLEEP_TIME)
             await self.sync_name()
 
     async def initalize(self):
         await sleep(5)
-        logger.info("Bắt đầu đếm member cho guildID %s", GUILDID)
+        self.bot.logger.info("Bắt đầu đếm member cho guildID %s", GUILDID)
         await self.sync_name()
+        self.sync_tasks = create_task(self.run_loop())
         try:
-            self.sync_tasks = create_task(self.run_loop())
+            await self.sync_tasks
         except CancelledError:
-            await self.bot.close()
+            pass
 
     @Cog.listener()
     async def on_ready(self):
         await self.initalize()
+
+    @Cog.listener("on_sync_failed")
+    async def handle_sync_failed_event(self):
+        self.sync_tasks.cancel()
+        self.bot.logger.error("Đã xảy ra sự cố khi đồng bộ member lên kênh")
 
 def setup(bot: ClientUser):
     bot.add_cog(MemberCount(bot))
